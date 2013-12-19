37a23d851729bff5d26e244a81e24fd44ef3ea50
Rodrigo Vaz <rodrigo.vaz@gmail.com>
2013-12-19 18:50:33 -0200

    prototype: better function separation and cleaner code

--- main.go
+++ main.go
@@ -9,41 +9,53 @@ import (
 	"os"
 )
 
+var base_url = "https://api.heroku.com"
+
 type Releases struct {
 	Id   string
 	Slug map[string]interface{}
 }
 
-func main() {
-	base_url := "https://api.heroku.com"
-	app_name := "ignite-heroku"
-
+func HttpClient() *http.Client {
 	tr := &http.Transport{
 		TLSClientConfig:    &tls.Config{},
 		DisableCompression: true,
 	}
 
 	client := &http.Client{Transport: tr}
+	return client
+}
 
-	request_url := fmt.Sprintf("%s/apps/%s/releases", base_url, app_name)
+func MakeReleaseRequest(app_name string, release string) *http.Request {
+	request_url := fmt.Sprintf("%s/apps/%s/releases/%s", base_url, app_name, release)
 	request, err := http.NewRequest("GET", request_url, nil)
 	if err != nil {
 		panic("request create")
 	}
-
 	request.Header.Add("Accept", "application/vnd.heroku+json; version=3")
 	request.SetBasicAuth(os.Args[1], os.Args[2])
+	return request
+}
 
+func GetSlugForRelease(app_name string, release_name string) string {
+	client := HttpClient()
+	request := MakeReleaseRequest(app_name, release_name)
 	resp, err := client.Do(request)
 	body, err := ioutil.ReadAll(resp.Body)
 
 	defer resp.Body.Close()
 
-	var release []Releases
+	var release Releases
 	err = json.Unmarshal(body, &release)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+v\n", release)
+	return release.Slug["id"].(string)
+}
+
+func main() {
+	app_name := "ignite-heroku"
+	release_name := "3"
+	fmt.Println(GetSlugForRelease(app_name, release_name))
 
 }
